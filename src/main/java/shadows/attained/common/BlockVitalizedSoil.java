400f1f83cbc2c900a854bfb3fb503ae89f47eaaf
p455w0rd <p455w0rd@gmail.com>
2016-11-26 02:33:47 -0500

    add block state logic to BlockVitalizedSoil

--- src/main/java/shadows/attained/common/BlockVitalizedSoil.java
+++ src/main/java/shadows/attained/common/BlockVitalizedSoil.java
@@ -2,37 +2,24 @@ package shadows.attained.common;
 
 import java.util.Random;
 
-
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyInteger;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.properties.*;
+import net.minecraft.block.state.*;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
+import net.minecraft.item.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import shadows.attained.AttainedDrops;
-import shadows.attained.ModRegistry;
+import net.minecraftforge.fml.relauncher.*;
+import shadows.attained.*;
 
-public class BlockVitalizedSoil extends Block{
-	 
+public class BlockVitalizedSoil extends Block {
 
-	//static final PropertyInteger ages;
-	
-	//@Override
-	//protected BlockStateContainer createBlockState(){
-	//	return new BlockStateContainer()
-	//}
-	
+	public static final PropertyInteger AGE = PropertyInteger.create("age", 0, 15);
+
 	public BlockVitalizedSoil() {
 		super(Material.GROUND);
 		setRegistryName("vitalizedsoil");
@@ -41,47 +28,70 @@ public class BlockVitalizedSoil extends Block{
 		setCreativeTab(ModRegistry.Attained);
 		setSoundType(SoundType.GROUND);
 		setUnlocalizedName(AttainedDrops.MODID + "vitalizedsoil");
-		setDefaultState(getStateFromMeta(0));
 		GameRegistry.register(this);
 		GameRegistry.register(new ItemBlock(this), getRegistryName());
+		setDefaultState(blockState.getBaseState().withProperty(getAgeProperty(), Integer.valueOf(0)));
 	}
 
-
 	@Override
-	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand)
-	{
+	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
 		super.updateTick(world, pos, state, rand);
-		int l = 0;
+		int l = getMetaFromState(state);
 
-		if (l < 15 && rand.nextInt(3) == 0)
-		{
+		if (l < 15 && rand.nextInt(3) == 0) {
 			l++;
 			int i = pos.getX() + (rand.nextInt(3) - 1);
 			int k = pos.getZ() + (rand.nextInt(3) - 1);
-			if (world.getBlockState(new BlockPos(i, pos.getY(), k)).getBlock() == Blocks.DIRT || world.getBlockState(new BlockPos(i, pos.getY(), k)).getBlock() == Blocks.GRASS)
-			{
+			if (world.getBlockState(new BlockPos(i, pos.getY(), k)).getBlock() == Blocks.DIRT || world.getBlockState(new BlockPos(i, pos.getY(), k)).getBlock() == Blocks.GRASS) {
 				world.setBlockState(new BlockPos(i, pos.getY(), k), ModRegistry.dummysoil.getDefaultState(), 2);
 				world.setBlockState(pos, getStateFromMeta(l), 2);
 			}
 		}
-		if (l == 15)
-		{
+		if (l == 15) {
 			world.setBlockState(pos, Blocks.DIRT.getDefaultState(), 2);
 		}
-	}	
-	
+	}
 
-	
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] {
+				AGE
+		});
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return withAge(meta);
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return getAge(state);
+	}
+
+	public int getMaxAge() {
+		return 7;
+	}
+
+	protected int getAge(IBlockState state) {
+		return state.getValue(getAgeProperty()).intValue();
+	}
+
+	public IBlockState withAge(int age) {
+		return getDefaultState().withProperty(getAgeProperty(), Integer.valueOf(age));
+	}
+
+	public boolean isMaxAge(IBlockState state) {
+		return state.getValue(getAgeProperty()).intValue() >= getMaxAge();
+	}
+
+	protected PropertyInteger getAgeProperty() {
+		return AGE;
+	}
+
 	@SideOnly(Side.CLIENT)
 	public void initModel() {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation("vitalizedsoil", "inventory"));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
