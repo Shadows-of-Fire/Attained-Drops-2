91b65126b5a9bfe33a6ba8ad5e3271618c6d6eaf
Jason Taylor <jason@codetaylor.com>
2016-12-23 14:59:00 -0800

    removed unused imports

--- src/main/java/shadows/attained/ModRegistry.java
+++ src/main/java/shadows/attained/ModRegistry.java
@@ -1,14 +1,6 @@
 package shadows.attained;
 
-import java.util.List;
-
-import net.minecraft.block.BlockDoublePlant;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import shadows.attained.common.BlockBulb;
