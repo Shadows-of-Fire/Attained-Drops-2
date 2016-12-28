68eae466fcf8beae99ae4e1b3a8779e908f241f9
p455w0rd <p455w0rd@gmail.com>
2016-12-28 17:31:50 -0500

    added function in buildscript to automatically get version from ModGlobals

--- src/main/java/shadows/attained/init/ModGlobals.java
+++ src/main/java/shadows/attained/init/ModGlobals.java
@@ -8,7 +8,7 @@ public class ModGlobals {
 
 	public static final String MODID = "attaineddrops";
 	public static final String MODNAME = "Attained Drops 2";
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "2.2.1";
 	public static final String CLIENT_PROXY = "shadows.attained.proxy.ClientProxy";
 	public static final String COMMON_PROXY = "shadows.attained.proxy.CommonProxy";
 
