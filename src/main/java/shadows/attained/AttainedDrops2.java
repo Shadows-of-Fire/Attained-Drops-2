65e0fc86738c1701208ff8a26b000ffd6ab28a57
Shadows_of_Fire <Bward7864@gmail.com>
2017-12-18 07:17:32 -0500

    update dep

--- src/main/java/shadows/attained/AttainedDrops2.java
+++ src/main/java/shadows/attained/AttainedDrops2.java
@@ -34,7 +34,7 @@ public class AttainedDrops2 {
 	public static final String MODID = "attaineddrops2";
 	public static final String MODNAME = "Attained Drops 2";
 	public static final String VERSION = "3.6.0";
-	public static final String DEPS = "required-after:placebo";
+	public static final String DEPS = "required-after:placebo@[1.1.0,)";
 
 	@SidedProxy(clientSide = "shadows.attained.proxy.ClientProxy", serverSide = "shadows.attained.proxy.CommonProxy")
 	public static CommonProxy PROXY;
