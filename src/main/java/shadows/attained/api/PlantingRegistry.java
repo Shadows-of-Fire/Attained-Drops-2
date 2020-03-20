494ec0ecd93a5e778d4bccb1959a9abaa31e1da9
Shadows_of_Fire <Bward7864@gmail.com>
2020-03-20 08:35:24 -0400

    add comments to custom properties file

--- src/main/java/shadows/attained/api/PlantingRegistry.java
+++ src/main/java/shadows/attained/api/PlantingRegistry.java
@@ -2,6 +2,7 @@ package shadows.attained.api;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -40,6 +41,9 @@ public class PlantingRegistry {
 		File f = new File(FMLPaths.CONFIGDIR.get().toFile(), "attained_drops_custom.properties");
 		if (!f.exists()) try {
 			f.createNewFile();
+			Properties props = new Properties();
+			String comment = "In this file, each new line entry is a new bulb type.  A line must contain the following information: name=item|color\r\n" + "Name: This is the internal name of this bulb type.  It may only use lowercase letters and numbers, and must be unique.\r\n" + "Item: This is the relevant item, used to vitalize soil and the item dropped by the bulb.  Must be an item registry name, such as minecraft:diamond\r\n" + "Color: This is a hex integer that represents the color of this soil/bulb combo.  It must be six digits long, and requires the 0x prefix.  Ex: 0xFFFFFF\r\n" + "An example definition for redstone is redstone=minecraft:redstone|0xAA0F01";
+			props.store(new FileOutputStream(f), comment);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
